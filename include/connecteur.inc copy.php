3df13b97a201e03833a3ee9629678ea460b0e586
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-22 21:42:40 +0100

    clean up

---
+++ include/connecteur.inc copy.php
@@ -0,0 +1,15 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 		 															 *
+ * Nom de la page :connecteur.inc.php								 *
+ * Date création :	28/04/2021										 *
+ * Date Modification : 28/04/2021									 *
+ * Créateur : Guillaume Crégut										 *													
+ * Version :	0.5 												 *
+ * Objet et notes :													 *
+ *	 Contient les informations de connexion à la base de données	 *
+ *	                              									 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+$DataBaseName = "YOUR_DB_NAME";
+$DataBaseUser = 'YOUR_DB_PASS';
+$DataBaseServeur='YOUR_DB_HOST';
