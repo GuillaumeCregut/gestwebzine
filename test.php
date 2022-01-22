0e2318c80b0ce776ce85230c2935a82cc3d5ac6c
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-22 21:40:43 +0100

    clean up

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $DateMessage = date("d/m/Y H:m", strtotime('2021-05-17 10:35:38'));
-  echo "<p >Date : $DateMessage </p>";
-  $MaDate=date_create('2021-05-17 10:35:38');
-   echo date_format($MaDate, 'd/m/Y H:i:s');
-?>
